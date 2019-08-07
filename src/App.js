d4e4fdac0b614ab17da8f8913ddda292433146ed
dbyilla69 <doranyilla@gmail.com>
2019-08-07 14:25:45 -0500

    file changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import Header from './components/header/header';
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in';
+import './sign-in-and-sign-up.styles.scss';
+
+const SingInAndSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SingInAndSignUpPage;
